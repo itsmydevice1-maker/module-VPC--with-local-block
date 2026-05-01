a3e77b4ca2ee6784ac579ce48a9071f483d65a5f
itsmydevice1-maker <itsmydevice1@gmail.com>
2026-05-01 22:27:25 +0530

    updated

--- local.tf
+++ local.tf
@@ -1,5 +1,6 @@
 locals {
     tags = {
         Name = "${var.environment}-infra project"
+        Environment = "dev"
     }
 }
--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   cidr = "11.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["11.0.0.0/24", "11.0.1.0/24", "11.0.2.0/24"]
+  private_subnets = ["11.0.0.0/24",]
   public_subnets  = ["11.0.5.0/24", "11.0.6.0/24", "11.0.7.0/24"]
 
   enable_nat_gateway = true
